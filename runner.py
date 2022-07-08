c8faa7c626721b62172ca29122c7c62d5645608b
Ashutosh Mishra <35286920+nityagautam@users.noreply.github.com>
2022-07-08 09:03:02 +0530

    Create runner.py
    Added the executioner

---
+++ runner.py
@@ -0,0 +1,21 @@
+# =============================================
+# Author           :
+# Created          : 08 July 2022
+# Last Modified    : 
+# Last Modified by :
+# Desc             : Runner of the automation framework
+# =============================================
+
+# Inside code integration goes here ...
+
+# Setup/Config integration goes here ...
+
+# test-data integration goes here ...
+
+
+# ================================
+# Execute the automation now
+# ================================
+if __name__ == "main":
+  pass
+
