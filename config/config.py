b812468feda2286873955817ffd447be61552488
Ashutosh Mishra <35286920+nityagautam@users.noreply.github.com>
2022-07-15 08:03:33 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,22 @@
+# @author: Ashutosh Mishra
+# created: 13 July 2022
 # This contains the config variables
+#
+
+# Collection order
+sanity_collections = []
+regreession_collections = []
+
+# data files
+test_data_files = {}
+
+# timeout in seconds
+timeout = 30
+
+# Binaries mapping
+os_binaries = {}
+
+# Error codes mapping
+error_codes = {}
+
+# 
