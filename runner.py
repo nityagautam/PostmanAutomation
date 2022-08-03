e9794de825148cc0ef31535ad1c5f275b653f2dd
Nitya Narayan Gautam <nityanarayan44@live.com>
2022-08-03 09:06:04 +0530

    newman test cmds

--- runner.py
+++ runner.py
@@ -107,7 +107,8 @@ if __name__ == "__main__":
     log.info(stdo)
     stdo, stde = Utilities().run_system_command("npm install newman --location=global")
     log.info(stdo)
-    stdo, stde = Utilities().run_system_command("c:/Users/nfaruqe/AppData/Roaming/npm/newman --version")
+    #stdo, stde = Utilities().run_system_command("c:/Users/nfaruqe/AppData/Roaming/npm/newman --version")
+    stdo, stde = Utilities().run_system_command("newman --version")
     log.info(stdo)
     log.info("")
 
